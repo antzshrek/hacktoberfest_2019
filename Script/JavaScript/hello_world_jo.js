528a20f76b0e00c4b5d7efbb467b9535545a0f21
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-10-22 12:34:53 -0700

    Added my Hello, World!
    Updated with my signature and code contribution.

---
+++ Script/JavaScript/hello_world_jo.js
@@ -0,0 +1,8 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Jo (jodoesgit)
+// GITHUB: https://github.com/jodoesgit
+
+const hello = ["1,","2,","3,","4...","Hello, World!","Tada!","\nI thank you!","*bows*"];
+
+hello.forEach((e)=>console.log(e));
